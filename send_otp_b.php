fbeadc5e6c87e8469229f9befa950dc3fce19d4b
Khushi1221 <82448070+Khushi1221@users.noreply.github.com>
2022-05-25 15:47:26 +0530

    Add files via upload

--- send_otp_b.php
+++ send_otp_b.php
@@ -1,11 +1,13 @@
 <?php
 session_start();
 
-use PHPMailer\PHPMailer\PHPMailer;
+//use PHPMailer\PHPMailer\PHPMailer;
+require_once('../sendgrid/config.php');
 require 'vendor/autoload.php';
-require 'vendor/PHPMailer/phpmailer/src/PHPMailer.php'; 
-require 'vendor/PHPMailer/phpmailer/src/SMTP.php';  
-require 'vendor/PHPMailer/phpmailer/src/Exception.php'; 
+require '../sendgrid/vendor/autoload.php';
+//require 'C:\xampp\htdocs\test\vendor\phpmailer\phpmailer\src\PHPMailer.php'; 
+//require 'C:\xampp\htdocs\test\vendor\phpmailer\phpmailer\src\SMTP.php';  
+//require 'C:\xampp\htdocs\test\vendor\phpmailer\phpmailer\src\Exception.php'; 
 
 $con = new MongoDB\Client('mongodb+srv://khushi:khushi123@cluster-nc.nm1fy.mongodb.net/ncdb?retryWrites=true&w=majority');
 $db = $con->ncdb;
@@ -14,25 +16,24 @@ $buyercoll = $db->buyer;
 
 if(isset($_POST['email']))
 {
-$email = $_POST['email'];
+$email1 = $_POST['email'];
 $e = ($_POST['email']);
 $_SESSION['em'] = $e;
 $res = $userscoll->findOne(["email" => $_POST['email']]);
-$resb = $buyercoll->findOne(["email" => $_POST['email']]);
 $count = $userscoll->count();
 if($count>0)
 {
-	if($res and $resb)
+	if($res)
 	{
 		
 		if(strcmp($_POST['password2'],$_POST['password3'])==0)
 		{
 			$otp = rand(100000,999999);
 			$userscoll->updateOne(["email" => $_POST['email']], ['$set' => ['otp' => $otp, "isexpired" => 0]]);
-			$_SESSION['EMAIL']=$email;
+			$_SESSION['EMAIL']=$email1;
 			$_SESSION['password2']=$_POST['password2'];
 			$_SESSION['password3']=$_POST['password3'];
-			sendOTP($email, $otp);
+			sendOTP($email1, $otp);
 			echo "Proceeding...";
 		}
 		else
@@ -51,31 +52,24 @@ else
 }
 }
 
-function sendOTP($email,$otp)
+function sendOTP($email1,$otp)
 	{	
-		$mail = new PHPMailer();
-		$mail->isSMTP();
-		$mail->Host = 'smtp.mailtrap.io';
-		$mail->SMTPAuth = true;
-		$mail->Port = 2525;
-		$mail->Username = '190fc72057e88b';
-		$mail->Password = '0f436579b26fc8';
-
-		$mail->setFrom('info@mailtrap.io', 'Mailtrap');
-		$mail->addReplyTo('info@mailtrap.io', 'Mailtrap');
-		
-		$message_body = "One Time Password for Admin Login is<br><br>".$otp;
-		$mail->AddAddress($email);
-		$mail->Subject = "OTP to Login";
-		$mail->MsgHTML($message_body);
-		$result = $mail->Send();
-		if(!$result)
+		$email = new \SendGrid\Mail\Mail(); 
+		$email->setFrom("newbiescompanion@gmail.com", "Newbie's Companion");
+		$email->setSubject("OTP to Reset Password");
+		$email->addTo($email1, "Newbie's Companion user");
+		$email->addContent("text/plain","Your One Time Password is ".$otp.".\n\nPlease do not share it with anyone.\n\nFor any queries, please write to us at newbiescompanion@gmail.com.");
+		$sendgrid = new \SendGrid( SENDGRID_API_KEY );
+		try 
 		{
-			echo "Mailer Error: ".$mail->ErrorInfo;
-		}
-		else
+			$response = $sendgrid->send($email);
+			//print $response->statusCode() . "\n";
+			//print_r($response->headers());
+			//print $response->body() . "\n";
+		} 
+		catch (Exception $e) 
 		{
-			return $result;
+			//echo 'Caught exception: '. $e->getMessage() ."\n";
 		}
 	}
 ?>
