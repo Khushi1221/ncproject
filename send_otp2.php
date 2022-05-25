a0cca4b53f838bc27b14c52fba14332bc24783f5
Khushi1221 <82448070+Khushi1221@users.noreply.github.com>
2022-05-25 15:48:32 +0530

    Update send_otp2.php

--- send_otp2.php
+++ send_otp2.php
@@ -1,1 +1,65 @@
-<?phpsession_start();//use PHPMailer\PHPMailer\PHPMailer;require_once('../sendgrid/config.php');require 'vendor/autoload.php';require '../sendgrid/vendor/autoload.php';//require 'C:\xampp\htdocs\test\vendor\phpmailer\phpmailer\src\PHPMailer.php'; //require 'C:\xampp\htdocs\test\vendor\phpmailer\phpmailer\src\SMTP.php';  //require 'C:\xampp\htdocs\test\vendor\phpmailer\phpmailer\src\Exception.php'; $con = new MongoDB\Client('mongodb+srv://khushi:khushi123@cluster-nc.nm1fy.mongodb.net/ncdb?retryWrites=true&w=majority');$db = $con->ncdb;$userscoll = $db->users;if(isset($_POST['email'])){$email1 = $_POST['email'];$e = ($_POST['email']);$_SESSION['em'] = $e;$res = $userscoll->findOne(["email" => $_POST['email']]);$count = $userscoll->count();if($count>0){	if($res)	{		$otp = rand(100000,999999);		$userscoll->updateOne(["email" => $_POST['email']], ['$set' => ['otp' => $otp, "isexpired" => 0]]);		$_SESSION['EMAIL']=$email1;		sendOTP($email1, $otp);		echo "Proceeding...";	}	else	{		echo "Email does not exists!";	}}else{	echo "Email does not exists!";}}function sendOTP($email1,$otp)	{		$email = new \SendGrid\Mail\Mail(); 		$email->setFrom("newbiescompanion@gmail.com", "Newbie's Companion");		$email->setSubject("OTP to Login");		$email->addTo($email1, "Newbie's Companion user");		$email->addContent("text/plain","Your One Time Password is ".$otp.".\n\nPlease do not share it with anyone.\n\nFor any queries, please write to us at newbiescompanion@gmail.com.");		$sendgrid = new \SendGrid( SENDGRID_API_KEY );		try 		{			$response = $sendgrid->send($email);			//print $response->statusCode() . "\n";			//print_r($response->headers());			//print $response->body() . "\n";		} 		catch (Exception $e) 		{			//echo 'Caught exception: '. $e->getMessage() ."\n";		}				}?>
+<?php
+session_start();
+
+//use PHPMailer\PHPMailer\PHPMailer;
+require_once('sendgrid/config.php');
+require 'vendor/autoload.php';
+require 'sendgrid/vendor/autoload.php';
+//require 'C:\xampp\htdocs\test\vendor\phpmailer\phpmailer\src\PHPMailer.php'; 
+//require 'C:\xampp\htdocs\test\vendor\phpmailer\phpmailer\src\SMTP.php';  
+//require 'C:\xampp\htdocs\test\vendor\phpmailer\phpmailer\src\Exception.php'; 
+
+$con = new MongoDB\Client('mongodb+srv://khushi:khushi123@cluster-nc.nm1fy.mongodb.net/ncdb?retryWrites=true&w=majority');
+$db = $con->ncdb;
+$userscoll = $db->users;
+
+if(isset($_POST['email']))
+{
+$email1 = $_POST['email'];
+$e = ($_POST['email']);
+$_SESSION['em'] = $e;
+$res = $userscoll->findOne(["email" => $_POST['email']]);
+$count = $userscoll->count();
+if($count>0)
+{
+	if($res)
+	{
+		$otp = rand(100000,999999);
+		$userscoll->updateOne(["email" => $_POST['email']], ['$set' => ['otp' => $otp, "isexpired" => 0]]);
+		$_SESSION['EMAIL']=$email1;
+		sendOTP($email1, $otp);
+		echo "Proceeding...";
+	}
+	else
+	{
+		echo "Email does not exists!";
+	}
+}
+else
+{
+	echo "Email does not exists!";
+}
+}
+
+function sendOTP($email1,$otp)
+	{
+		$email = new \SendGrid\Mail\Mail(); 
+		$email->setFrom("newbiescompanion@gmail.com", "Newbie's Companion");
+		$email->setSubject("OTP to Login");
+		$email->addTo($email1, "Newbie's Companion user");
+		$email->addContent("text/plain","Your One Time Password is ".$otp.".\n\nPlease do not share it with anyone.\n\nFor any queries, please write to us at newbiescompanion@gmail.com.");
+		$sendgrid = new \SendGrid( SENDGRID_API_KEY );
+		try 
+		{
+			$response = $sendgrid->send($email);
+			//print $response->statusCode() . "\n";
+			//print_r($response->headers());
+			//print $response->body() . "\n";
+		} 
+		catch (Exception $e) 
+		{
+			//echo 'Caught exception: '. $e->getMessage() ."\n";
+		}
+			
+	}
+?>
